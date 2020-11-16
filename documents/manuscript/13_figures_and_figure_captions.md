0072c4e5bf620ebb2e11ca61336eac4221e66443
Matthew L. Bowser <matt_bowser@fws.gov>
2020-11-16 08:01:17 -0900

    Add content to study site section

---
+++ documents/manuscript/13_figures_and_figure_captions.md
@@ -0,0 +1,3 @@
+
+# Figures and Figure Captions
+
--- documents/manuscript/convert.sh
+++ documents/manuscript/convert.sh
@@ -1,1 +1,18 @@
-pandoc *.md --filter pandoc-citeproc --reference-doc=custom-reference.docx -o manuscript.docx
+pandoc 01_title.md \
+02_author_information.md \
+03_abstract.md \
+04_introduction.md \
+05_study_site.md \
+06_methods.md \
+07_results.md \
+08_discussion.md \
+10_acknowledgments.md \
+11_references.md \
+ --filter pandoc-citeproc --reference-doc=custom-reference.docx -o manuscript_01.docx
+
+pandoc 13_figures_and_figure_captions.md --reference-doc=custom-reference.docx -o manuscript_02.docx
+
+pandoc manuscript_01.docx manuscript_02.docx --reference-doc=custom-reference.docx -o manuscript.docx
+
+rm manuscript_01.docx
+rm manuscript_02.docx
