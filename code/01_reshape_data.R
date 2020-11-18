a1050183d3b31dba4fcd6ca926952eff5e627314
Matthew L. Bowser <matt_bowser@fws.gov>
2020-11-17 20:55:25 -0900

    Improve data summaries

--- code/01_reshape_data.R
+++ code/01_reshape_data.R
@@ -1,20 +1,33 @@
 
 ## Reshape data
 
+library(knitr)
+
 data01 <- read.csv("../data/raw_data/occurrence_data/2020-11-12-1400_occurrences.csv", stringsAsFactors=FALSE)
+
+out_file <- paste0("../documents/", format(Sys.time(), format="%Y-%m-%d-%H%M"), "_data_summaries.md")
 
 ## How many records were there?
 nrow(data01)
-#[1] 2375
+
+write("
+Number of records: \\ ", out_file)
+write(nrow(data01), out_file, append=TRUE)
 
 ## How many unique identifications were there?
 length(unique(data01$SCIENTIFIC_NAME))
-#[1] 975
+
+write("
+Number of unique identifications: \\ ", out_file, append=TRUE)
+write(length(unique(data01$SCIENTIFIC_NAME)), out_file, append=TRUE)
 
 ## Are all of the species X event records unique? (It is possible that some MOTUs ended up with the same identificaions.)
 nrow(unique(data01[,c("SCIENTIFIC_NAME", "SPEC_LOCALITY", "BEGAN_DATE")]))
-#[1] 2375
 ## Yes, it appears that they are all unique.
+
+write("
+Number of unique species X event records: \\ ", out_file, append=TRUE)
+write(length(unique(data01$SCIENTIFIC_NAME)), out_file, append=TRUE)
 
 ## I think it will be most handy to use Julian dates.
 data01$BEGAN_DATE <- as.Date(data01$BEGAN_DATE)
@@ -24,9 +37,14 @@ data01$julian_day <- as.numeric(format(data01$BEGAN_DATE, "%j"))
 ag01 <- aggregate(data01$GUID, by=list(data01$julian_day), length)
 names(ag01) <- c("julian_day", "n_observations")
 
+write("
+Number of records by Julian day", out_file, append=TRUE)
+write(kable(ag01), out_file, append=TRUE)
+
 ## Plot number of observations versus Julian day.
+image_file <- paste0("../documents/images/", format(Sys.time(), format="%Y-%m-%d-%H%M"), "_observations_vs_julian_day.png")
 width <- 600
-png(filename=paste0("../documents/images/", format(Sys.time(), format="%Y-%m-%d-%H%M"), "_observations_vs_julian_day.png"),
+png(filename=image_file,
  width=width,
  height=round(width/1.618),
  pointsize=12
@@ -41,5 +59,11 @@ plot(ag01$julian_day,
  )
 dev.off()
 ## It might make more sense to plot the number of observations per unit effort.
+
+image_caption <- "Numbers of observation records by Julian day."
+write(paste0("
+![", image_caption, "](", gsub("../documents/", "", image_file), ")\\ 
+", image_caption, "
+"), out_file, append=TRUE)
 
 
---
+++ documents/2020-11-17-2050_data_summaries.md
@@ -0,0 +1,32 @@
+
+Number of records: \ 
+2375
+
+Number of unique identifications: \ 
+975
+
+Number of unique species X event records: \ 
+975
+
+Number of records by Julian day
+| julian_day| n_observations|
+|----------:|--------------:|
+|        166|            356|
+|        167|            290|
+|        168|            212|
+|        169|            225|
+|        200|             77|
+|        201|            153|
+|        202|            129|
+|        203|             96|
+|        204|            164|
+|        208|             27|
+|        211|             93|
+|        214|             67|
+|        216|            167|
+|        222|            226|
+|        223|             93|
+
+![Numbers of observation records by Julian day.](images/2020-11-17-2050_observations_vs_julian_day.png)\ 
+Numbers of observation records by Julian day.
+
