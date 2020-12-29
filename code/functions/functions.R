c8f6d1144c27c64f8f211f7446a46872a6e448f2
Matthew L. Bowser <matt_bowser@fws.gov>
2020-12-29 11:38:35 -0900

    Add NLCD and simulations

--- code/functions/functions.R
+++ code/functions/functions.R
@@ -9,4 +9,7 @@ nowstring <- function()
  {
  format(Sys.time(), format="%Y-%m-%d-%H%M")
  }
- 
+
+## Projections.
+albers <- "+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+wgs84 <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" 
